8e85beedec973d3f8b9b08cd63ef11da8e0db238
Khan Kene <ken.sap@gis-expert.kz>
2021-09-21 15:45:57 +0500

    Добавлено класс UUID

---
+++ src/base/uuid.ts
@@ -0,0 +1,18 @@
+import { v4, validate } from 'uuid';
+
+export class UUID {
+  protected constructor(protected readonly rawUuid: string) {}
+
+  static create(rawUuid: string): UUID | undefined;
+
+  static create(): UUID;
+
+  static create(rawUuid?: string): UUID | undefined {
+    if (rawUuid && !validate(rawUuid)) return undefined;
+    return new UUID(rawUuid || v4());
+  }
+
+  getRawUuid(): string {
+    return this.rawUuid;
+  }
+}
