9fb391fb35978159e260d86062e35963a1ab852b
Khan Kene <ken.sap@gis-expert.kz>
2021-09-21 16:30:40 +0500

    Изменения в EventMessage

--- src/message/event-message.ts
+++ src/message/event-message.ts
@@ -1,14 +1,14 @@
 import { UUID } from '../base/uuid';
 import { Serializable } from '../serializable/serializable';
-import { SerializableCapsule } from '../serializable/serializable-capsule/serializable-capsule';
+import { InstanceSC } from '../serializable/serializable-capsule/instance-serializable-capsule';
 import { Message, MessageProps } from './message';
 
-export interface EventMessageProps<P extends SerializableCapsule<Serializable>> extends MessageProps<P> {
+export interface EventMessageProps<P extends Serializable> extends MessageProps<InstanceSC<P>> {
   aggregateType: string;
   aggregateId: UUID;
 }
 
-export class EventMessage<P extends SerializableCapsule<Serializable>> extends Message<P, EventMessageProps<P>> {
+export class EventMessage<P extends Serializable> extends Message<InstanceSC<P>, EventMessageProps<P>> {
   getAggregateType(): string {
     return this.props.aggregateType;
   }
