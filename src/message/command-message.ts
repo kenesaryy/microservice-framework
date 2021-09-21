6bb1e9c17e1d0857eea6e1200bba663fcf91635b
Khan Kene <ken.sap@gis-expert.kz>
2021-09-21 16:28:57 +0500

    Изменения в CommandMessage

--- src/message/command-message.ts
+++ src/message/command-message.ts
@@ -1,8 +1,8 @@
 import { Serializable } from '../serializable/serializable';
-import { SerializableCapsule } from '../serializable/serializable-capsule/serializable-capsule';
+import { InstanceSC } from '../serializable/serializable-capsule/instance-serializable-capsule';
 import { Message, MessageProps } from './message';
 
-export interface CommandMessageProps<P extends SerializableCapsule<Serializable>> extends MessageProps<P> {}
+export interface CommandMessageProps<P extends Serializable> extends MessageProps<InstanceSC<P>> {}
 
-export class CommandMessage<P extends SerializableCapsule<Serializable>> extends Message<P, CommandMessageProps<P>> {
+export class CommandMessage<P extends Serializable> extends Message<InstanceSC<P>, CommandMessageProps<P>> {
 }
