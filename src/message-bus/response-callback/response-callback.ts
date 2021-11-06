4ff3ef036c7b640eb29c804dcf78a5f51c116f95
Khan Kene <ken.sap@gis-expert.kz>
2021-11-06 14:24:34 +0500

    Добавлено ResponseCallback

---
+++ src/message-bus/response-callback/response-callback.ts
@@ -0,0 +1,76 @@
+import { CommandMessage } from '../../message/command-message';
+import { QueryMessage } from '../../message/query-message';
+import { ResponseMessage } from '../../message/response-message';
+import { Serializable } from '../../serializable/serializable';
+import { SerializableCapsule } from '../../serializable/serializable-capsule/serializable-capsule';
+
+interface HandleLogic<
+  M extends CommandMessage<Serializable> | QueryMessage<Serializable>,
+  SC extends SerializableCapsule<Serializable>
+> {
+  (message: M, resp: ResponseMessage<SC>): Promise<void>;
+}
+
+interface ErrorHandleLogic<ERR> {
+  (err: ERR): Promise<void>;
+}
+
+export class ResponseCallback<
+  M extends CommandMessage<Serializable> | QueryMessage<Serializable>,
+  ERR
+> {
+  // Порядок регистрации responseTypeAndHandleLogic важен
+  private responseTypeAndHandleLogic: [SerializableCapsule<Serializable>, HandleLogic<M, SerializableCapsule<Serializable>>][] = [];
+  private onErrorHandler?: ErrorHandleLogic<any>;
+
+  onResponse<SC extends SerializableCapsule<Serializable>>(
+    responseType: SC,
+    handleLogic: HandleLogic<M, SC>,
+  ): void {
+    // TODO надо добавить проверку дублирования
+    this.responseTypeAndHandleLogic.push(
+      [
+        responseType,
+        handleLogic as HandleLogic<M, SerializableCapsule<Serializable>>,
+      ]
+    );
+  }
+
+  onError(errorHandleLogic: ErrorHandleLogic<ERR>): void {
+    if (this.onErrorHandler) throw new Error('Повторная попытка регистрировать ErrorHandleLogic');
+    this.onErrorHandler = errorHandleLogic;
+  }
+
+  async responseReceived(
+    message: M,
+    resp: ResponseMessage<SerializableCapsule<Serializable>>
+  ): Promise<void> {
+    const handleLogic = this.getMatchedHandleLogic(resp.getPayload());
+    if (!handleLogic) throw new Error(); // TODO
+    await handleLogic(
+      message,
+      resp,
+    );
+  }
+
+  async errorReceived(
+    err: ERR,
+  ): Promise<void> {
+    if (!this.onErrorHandler) throw new Error(); // TODO
+    await this.onErrorHandler(err);
+  }
+
+  private getMatchedHandleLogic<SC extends SerializableCapsule<Serializable>>(
+    respPayload: SC,
+  ): HandleLogic<M, SC> | undefined {
+    let res: HandleLogic<M, SC> | undefined = undefined;
+    this.responseTypeAndHandleLogic.some((value) => {
+      if (value[0].match(respPayload)) {
+        res = value[1];
+        return true;
+      }
+      return false;
+    })
+    return res;
+  }
+}
