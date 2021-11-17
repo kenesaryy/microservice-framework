e5236109d568cff263ec8a81985b3104d0f35c2d
Khan Kene <ken.sap@gis-expert.kz>
2021-11-17 15:03:41 +0500

    Добавлены типы GeneralMessage, HandlableMessage

--- src/message/types.ts
+++ src/message/types.ts
@@ -1,6 +1,13 @@
+import { Serializable } from '../serializable/serializable';
+import { InstanceSC } from '../serializable/serializable-capsule/instance-serializable-capsule';
 import { SerializableCapsule } from '../serializable/serializable-capsule/serializable-capsule';
-import { Message } from './message';
+import { Message, MessageProps } from './message';
 
 export type InferMessageBusSerializable<
   M extends Message<any, any>,
 > = M extends Message<SerializableCapsule<infer S>, any> ? S : never;
+
+export type GeneralMessage = Message<InstanceSC<Serializable>, MessageProps<InstanceSC<Serializable>>>;
+
+export type HandlableMessage<S extends Serializable> =
+  Message<InstanceSC<S>, MessageProps<InstanceSC<S>>>;
