99234cd993e415446612f3059cbe675b90507df1
Khan Kene <ken.sap@gis-expert.kz>
2021-12-02 11:46:20 +0500

    Добавлено логика по dispatch interceptor в MessageBus

--- src/message-bus/message-bus.ts
+++ src/message-bus/message-bus.ts
@@ -1,15 +1,20 @@
 import { Constructor } from '../base/constructor';
-import { Handler } from '../handler/handler';
-import { Message, MessageProps } from '../message/message';
+import { Queue } from '../base/utils/queue';
+import { GeneralHandler, InferMessageFromHandler } from '../handler/types';
 import { Serializable } from '../serializable/serializable';
-import { InstanceSC } from '../serializable/serializable-capsule/instance-serializable-capsule';
 import { HandlerRunningLogic } from './handler-running-logic/handler-running-logic';
+import { MessageDispatchInterceptor } from './interceptor/message-dispatch-interceptor';
+import { MessageHandlerInterceptor } from './interceptor/message-handler-interceptor';
 
 export abstract class MessageBus<
   S extends Serializable,
-  H extends Handler<Message<InstanceSC<S>, MessageProps<InstanceSC<S>>>, any>
+  H extends GeneralHandler
 > {
   protected handlerRunningLogic: HandlerRunningLogic = new HandlerRunningLogic(); // TODO
+
+  protected readonly handleInterceptors: Queue<MessageHandlerInterceptor<H>> = [];
+
+  protected readonly dispatchInterceptors: Queue<MessageDispatchInterceptor<InferMessageFromHandler<H>>> = [];
 
   abstract subscribe<
     SERIALIZABLE extends S,
@@ -17,4 +22,15 @@ export abstract class MessageBus<
   >(
     sConstr: Constructor<SERIALIZABLE>, handler: HANDLER,
   ): Promise<void>;
+
+  async intercept<M extends InferMessageFromHandler<H>>(message: M): Promise<M> {
+    const dIs = this.dispatchInterceptors.values();
+    let res = message;
+    while(true) {
+      const interceptor = dIs.next();
+      if (interceptor.done) break;
+      res = await interceptor.value.handle(res) as M;
+    }
+    return res;
+  }
 }
